78144dee27c9f800e477ad32c127ca40ce90f4bb
MaviyaQureshi <maviyaqureshi2003@gmail.com>
2022-09-20 14:39:17 +0530

    first commit

--- root/index.js
+++ root/index.js
@@ -1,11 +1,7 @@
 var username = document.getElementsByClassName("name").value;
 var password = document.getElementsByClassName("word").value; 
 function validate(){
-    if(username == " "){
-        alert("Please enter an Email!");
-    }else if(password == " "){
-        alert("Please enter a Password!");
-    }else if(username == "" && password == " "){
+     if(username == "" || password == ""){
         alert("Please enter the details!");
     }
 };
